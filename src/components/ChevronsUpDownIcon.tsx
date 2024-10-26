10cedf785af6488bdcfcb77d35cc15be505bfc20
vivek bhardwaj <vivekb153@gmail.com>
2024-10-26 03:25:56 -0400

    add: chevron up down icon

---
+++ src/components/ChevronsUpDownIcon.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ChevronsUpDownIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="m7 15 5 5 5-5" />
+    <path d="m7 9 5-5 5 5" />
+  </svg>
+);
+
+export default ChevronsUpDownIcon;
+
